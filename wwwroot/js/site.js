0955037cbb45e5491437af4ebcae572e41e0dfe6
Hernanda <m.hernanda95@gmail.com>
2021-09-12 20:17:07 +0700

    Initial commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+window.addEventListener('scroll', function () {
+    if (this.scrollY == 0)
+        document.getElementById('nav').className = "navtop";
+    else
+        document.getElementById('nav').className = "navOnScroll";
+
+});
